674f28ba7e18805785b3b19b93e85b209f1cabbf
Nick <nsantander1095@gmail.com>
2022-09-15 22:32:44 -0400

    got everything working, luckily the modular pathing demo helped a bunch

---
+++ routes/notes.js
@@ -0,0 +1,28 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            tip_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added successfully');
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
